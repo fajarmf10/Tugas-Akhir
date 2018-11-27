faeb75e7a2e1196f19835c298c4337595de1413a
Fajar Maulana Firdaus <sosispanggang.15@gmail.com>
2018-11-27 19:14:45 +0700

    MUMET NDASKU COK

--- mbak Raras/Port-Py/3d-region-merging/srm3d.py
+++ mbak Raras/Port-Py/3d-region-merging/srm3d.py
@@ -1,4 +1,6 @@
 import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
 
 def srm3d(I, Q, dim):
     if len(locals()) <= 2:
@@ -8,5 +10,18 @@ def srm3d(I, Q, dim):
     smallest_region_allowed = 1
     size_image = [np.shape(I)[0], np.shape(I)[1]]
     if(dim==2):
-        [Ix, Iy] = 
+        [Ix, Iy] = [cv.Sobel(I, cv.CV_64F,1,0), cv.Sobel(I, cv.CV_64F,0,1)]
+        normgradient = np.sqrt(Ix ** 2.0 + Iy ** 2.0)
+        # Ixx = plt.imshow(Ix)
+        # plt.show()
+        # Iyy = plt.imshow(Iy)
+        # plt.show()
+        Ix = np.delete(Ix, 0, -1)
+        Iy = np.delete(Iy, -1, 0)
+        # BADDDDDDD MOODDDDDD
+        index = np.append(np.abs(Iy.flatten(1)), np.abs(Ix.flatten(1)))
+        # index = np.abs([Iy.flatten(1)])
+        # index = np.append(index, )
+
+        # index = np.sort(np.abs([Iy.flatten(1); Ix.flatten(1)]))
     print('A')
